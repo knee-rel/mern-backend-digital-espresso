2a59aec75c6050b270659419311323c491f104d4
Nirel Ibarra <62606372+knee-rel@users.noreply.github.com>
2023-09-16 21:33:43 +0800

    sending POST request to the backend

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Acces-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
 app.use("/api/places", placesRoutes); // => /api/places...
 app.use("/api/users", usersRoutes);
 
@@ -30,7 +41,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    app.listen(5001);
+    app.listen(5000);
   })
   .catch((err) => {
     console.log(err);
