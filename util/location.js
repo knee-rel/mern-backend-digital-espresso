05405fa09537e5c848c69a63b310cace09f63687
Nirel Ibarra <62606372+knee-rel@users.noreply.github.com>
2024-02-05 18:38:57 +0800

    finalized env variables

--- util/location.js
+++ util/location.js
@@ -1,46 +1,11 @@
-// const axios = require("axios");
-
-// const HttpError = require("../models/http-error");
-
-// const API_KEY = "AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA";
-
-// async function getCoordsForAddress(address) {
-//   // return {
-//   //   lat: 40.7484474,
-//   //   lng: -73.9871516
-//   // };
-//   const response = await axios.get(
-//     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-//       address
-//     )}&key=${API_KEY}`
-//   );
-
-//   const data = response.data;
-
-//   if (!data || data.status === "ZERO_RESULTS") {
-//     const error = new HttpError(
-//       "Could not find location for the specified address.",
-//       422
-//     );
-//     throw error;
-//   }
-
-//   const coordinates = data.results[0].geometry.location;
-
-//   return coordinates;
-// }
-
-// module.exports = getCoordsForAddress;
-
 const axios = require("axios");
 const HttpError = require("../models/http-error");
-const API_KEY = "pk.08c3f1ff6a4710cb84f262248bff2ded";
 
 async function getCoordsForAddress(address) {
   const response = await axios.get(
-    `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${encodeURIComponent(
-      address
-    )}&format=json`
+    `https://us1.locationiq.com/v1/search.php?key=${
+      process.env_LOCATION_IQ_KEY
+    }&q=${encodeURIComponent(address)}&format=json`
   );
 
   const data = response.data[0];
